44570c369eea2fdd5d4eb975260dd38ee3d1040c
SavindiLayanga <savindilayanga@gmail.com>
2024-10-02 17:26:10 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css'
 import Home from "./pages/home";
 import NavBar from "./components/navBar.tsx";
 import About from "./pages/about.tsx";
+import Skills from "./pages/skills.tsx";
 import Expertise from "./pages/expertise.tsx";
 import Contact from "./pages/contact.tsx";
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <NavBar/>
           <Home/>
           <About/>
+          <Skills/>
           <Expertise/>
           <Contact/>
       </div>
